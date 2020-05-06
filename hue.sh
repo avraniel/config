002ccade581fec6743d7c26d124627f93961d664
avraniel <avraniel@gmail.com>
2020-05-06 20:28:46 +0200

    update

--- hue.sh
+++ hue.sh
@@ -2,6 +2,6 @@
 #set -e
 
 
-wal -c; wal -i /home/niel/DATA/wallpapers/; feh --bg-scale "$(< "${HOME}/.cache/wal/wal")"
+wal -c; wal -i /home/niel/DATA/wallpapers/; feh --bg-fill "$(< "${HOME}/.cache/wal/wal")"
 
 #echo "done"
