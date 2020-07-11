106408fa87c7cdec783db0d3a88fa83bb22f8147
avraniel <avraniel@gmail.com>
2020-07-11 20:28:31 +0200

    update

--- toolsko.sh
+++ toolsko.sh
@@ -36,7 +36,7 @@ yay -S zafiro-icon-theme --noconfirm
 yay -S xcursor-simpleandsoft --noconfirm
 yay -S popcorntime-bin --noconfirm
 yay -S oh-my-bash-git --noconfirm --needed
-yay -S otf-fira-code --noconfirm --needed
+yay -S ttf-fira-code --noconfirm --needed
 yay -S arcolinux-logout-themes-git --noconfirm --needed
 
 
