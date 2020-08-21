7a1ca946adc4c4a0f743cf6ce95aba7575ab828c
avraniel <avraniel@gmail.com>
2020-08-21 15:27:25 +0200

    update

--- hue.sh
+++ hue.sh
@@ -3,6 +3,9 @@
 
 
 wal -c; wal -i /home/niel/DATA/wallpapers/; feh --bg-fill "$(< "${HOME}/.cache/wal/wal")"; spicetify update; spicetify apply -n; Pywalfox/./daemon/pywalfox.py update
-
+# wal -c; wal -i /home/niel/DATA/wallpapers/; nitrogen --random --set-auto /home/niel/DATA/wallpapers/; "$(< "${HOME}/.cache/wal/wal")"
 
 #echo "done"
+
+
+
